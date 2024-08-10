58684f9c971cdb0690e753467506952deae68b93
Rinky Singh <rinky.rakesh@gmail.com>
2024-08-11 00:31:48 +0530

    loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,19 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
     }
-      ? api_key=${API_KEY}& language=en-US&page=1`
+      ? api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10 } }
   );
+
   const data = await res.json();
   if (!res.ok) {
-     throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
   const results = data.results;
-  console.log(results);
 
-  return  <div>
-  <Results results={results}/>
-  </div>
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
